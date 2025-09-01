fb3ae37709fe9faf6309cfa328053dabc43f3dd8
benjaminleichter <benleichter@gmail.com>
2025-08-31 19:45:00 -0700

    update height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <div className="min-h-screen relative bg-off-white">
+    <div className="min-h-screen relative bg-off-white flex flex-col items-center">
       <Navigation />
       <Hero />
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -79,7 +79,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section id="home" className="min-h-screen flex items-center justify-center relative z-10">
+    <section id="home" className="flex items-center flex-grow-1 justify-center relative z-10">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold text-gray-900 mb-6">
           Hi, I&apos;m <span ref={benRef} className="inline-block absolute align-baseline">Ben</span><span className="invisible">Ben</span>
